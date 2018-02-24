508a99b335d2952f16c7b2f7b211efffb0929fc5
Raoul1996 <baobaozhou199606@gmail.com>
2018-02-24 17:38:41 +0800

    first test

---
+++ app.js
@@ -0,0 +1,10 @@
+'use strict';
+const Captcha = require('./lib/captcha');
+module.exports = app => {
+  const captcha = new Captcha();
+  app.captcha = {
+    generate() {
+      return captcha.generate();
+    },
+  };
+};
--- config/config.default.js
+++ config/config.default.js
@@ -6,5 +6,13 @@
  * @property {String} SOME_KEY - some description
  */
 exports.captcha = {
+  width: 256, // set width,default is 256
 
+  height: 60, // set height,default is 60
+
+  offset: 40, // set text spacing,default is 40
+
+  quality: 100, // set pic quality,default is 50
+
+  fontsize: 57, // set font size,default is 57
 };
