0103c0788939de0c0539cb76c59525f22d86aa1e
Raoul1996 <baobaozhou199606@gmail.com>
2019-10-01 01:12:54 +0800

    Fix missing config error
    Change-Id: I6e703020324eb521fb093d828fd2df4c03c3d954

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 'use strict';
 const Captcha = require('./lib/captcha');
 module.exports = app => {
-  const captcha = new Captcha();
+  const captcha = new Captcha({});
   app.captcha = {
     generate() {
       return captcha.generate();
